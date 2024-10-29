d8f1f0c90f744eebd33076e7c28ab37a4737cfa8
ewpimenov <sherlokprs@gmail.com>
2024-10-29 22:15:12 +0300

    commit

--- src/main/java/com/example/reminder/repository/ReminderRepository.java
+++ src/main/java/com/example/reminder/repository/ReminderRepository.java
@@ -4,7 +4,6 @@ import com.example.reminder.model.Reminder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ReminderRepository extends JpaRepository<Reminder, Integer> {
 
